440f56c7b4214fca5eff07d49c6a989ce5d59cd0
ramakanth47 <105962313+ramakanth47@users.noreply.github.com>
2022-11-05 13:02:54 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Copyright © divili all rights reserved. Powered by ramakanth.</p>
+      </footer>
+    </>
+  )
+}
